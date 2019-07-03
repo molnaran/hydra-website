ece49a62e1af7ea5cb6d68589ae7d8346b05bd9b
molnaran <molnar.andras03@gmail.com>
2019-07-03 15:40:29 +0200

    Added JWT Authentication

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const history = require("./routes/api/history");
 
 const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const db = require("./config/keys").mondoURI;
 
@@ -13,9 +19,11 @@ mongoose
   .then(() => console.log("MonboDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
 
 app.use("/api/users", users);
 app.use("/api/history", history);
